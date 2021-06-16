d72b38afbdb4fe9d98de48ed920c4fbe7498d49b
Lodewyk Duminy <lodewyk.duminy@gmail.com>
2021-06-16 17:02:43 +1200

    convertSingle test
    Added a single working test for convertSingle

---
+++ convertSingle.test.js
@@ -0,0 +1,5 @@
+const convertSingle = require(`./convertSingle`)
+
+test('one', () => {
+    expect(convertSingle.convertSingle(`1`)).toBe(`one`)
+})
--- convertTens.test.js
+++
@@ -1,5 +0,0 @@
-const convertTens = require(`./convertTens`)
-
-test(`ten`, () => {
-    expect(convertTens.convertTens(`1`, `0`)).toBe(`ten`)
-})
